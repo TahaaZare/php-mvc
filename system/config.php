fb08894ae30cd550602d16b636d4876b57d836c9
Taha Zare <tahaazre@gmail.com>
2022-09-03 02:05:59 +0430

    first commit

---
+++ system/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$base_url = "http://localhost/php-mvc/";
+$base_dir = "/php-mvc/";
+
+$tmp = explode('?', $_SERVER['REQUEST_URI']);
+$current_route = str_replace($base_dir, '', $tmp[0]);
+unset($tem);
