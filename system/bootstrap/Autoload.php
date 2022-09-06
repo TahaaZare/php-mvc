9ddcd982997edcbbbe582a9c027d59321b7331e0
Taha Zare <tahaazre@gmail.com>
2022-09-06 21:55:56 +0430

    Auto Load 1-Fixed codes in Index.php 2-create auto loader class for classes

---
+++ system/bootstrap/Autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace System\Bootstrap;
+
+
+class Autoload
+{
+    public function autoloader()
+    {
+        spl_autoload_register(function ($className) {
+            $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
+            include_once $_SERVER['DOCUMENT_ROOT'] . '/php-mvc/' . $className . '.php';
+        });
+    }
+}
--- system/bootstrap/boot.php
+++ system/bootstrap/boot.php
@@ -1,7 +1,15 @@
 <?php
 
+use System\Bootstrap\Autoload;
 use System\Router\Routing;
 
-require('system/config.php');
+session_start();
+
+require_once('system/config.php');
+require_once('system/bootstrap/Autoload.php');
+
+$autoload = new Autoload();
+$autoload->autoloader();
+
 $router = new Routing();
 $router->run();
