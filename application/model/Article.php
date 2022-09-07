7be9a3bf1a10b18416308e5abe32cc4bc2685259
Taha Zare <tahaazre@gmail.com>
2022-09-07 18:52:44 +0430

    #1 Add Models 1-Article Model with Methods 2-Category Model with Methods

---
+++ application/model/Article.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Model;
+
+class Article extends Model
+{
+    public function all()
+    {
+    }
+
+    public function find($id)
+    {
+    }
+
+    public function insert($values)
+    {
+    }
+
+    public function update($id, $values)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
+}
