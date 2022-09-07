d4030233f73c00502a291a1a49936bd1444ddadb
Taha Zare <tahaazre@gmail.com>
2022-09-08 01:11:15 +0430

    Complate Article Model

--- application/model/Article.php
+++ application/model/Article.php
@@ -6,21 +6,40 @@ class Article extends Model
 {
     public function all()
     {
+        $query = "SELECT * FROM `articles`;";
+        $result = $this->query($query)->fetchAll();
+        $this->closeConnection();
+        return $result;
     }
 
     public function find($id)
     {
+        $query = "SELECT *, (SELECT `name` FROM `categories` WHERE `categories`.`id` = `articles`.`cat_id`) as `category`
+            FROM `articles` WHERE id = ?";
+        $result =  $this->query($query)->fetch();
+        $this->closeConnection();
+        return $result;
     }
 
     public function insert($values)
     {
+        $query = "INSERT INTO `articles` (`title`,`cat_id`,`body`,`created_date`) VALUES (?,?,?, now());";
+        $this->execute($query, array_values($values));
+        $this->closeConnection();
     }
 
     public function update($id, $values)
     {
+        $query = "UPDATE `articles` SET `title` = ?, `cat_id` = ?, `body` = ?, `updated_date` = now()
+                WHERE `id` = ?;";
+        $this->execute($query, array_merge(array_values($values), [$id]));
+        $this->closeConnection();
     }
 
     public function delete($id)
     {
+        $query = "DELETE FROM `articles` WHERE `id` = ?;";
+        $this->execute($query, [$id]);
+        $this->closeConnection();
     }
 }
